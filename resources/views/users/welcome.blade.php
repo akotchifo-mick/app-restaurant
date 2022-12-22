9200741fba3e551d91d8bbe007a44481061238b8
Lordis Adjele <lordis.adjele@gmail.com>
2022-12-22 13:05:49 +0000

    more suitable design

--- resources/views/users/welcome.blade.php
+++ resources/views/users/welcome.blade.php
@@ -4,7 +4,6 @@
 @section('nav-menu')
 <li class="scroll-to-section"><a href="#about">@lang('About')</a></li>
 <li class="scroll-to-section"><a href="#chefs">Menu</a></li>
-<li class="scroll-to-section"><a href="#reservation">Réserver</a></li>
 @auth
 <li>
     <livewire:ticket-button />
@@ -113,52 +112,6 @@
 </div>
 <!-- ***** Connexion Modal Ends ***** -->
 
-
-<!-- ***** Reservation Modal Starts ***** 
-        <div class="modal" id="reservationModal" data-backdrop="static">
-            <div class="modal-dialog modal-lg modal-dialog-centered row">
-                <div id="top">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <div class="left-content">
-                                    <div class="inner-content">
-                                        <h4>Restau-U</h4>
-                                        <h6>Plus simple &amp; Plus efficace</h6>
-                                        <div class="main-white-button scroll-to-section">
-                                            <a href="#reservation">@lang('Book-Table')</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-8">
-                                <div class="main-banner header-text">
-                                    <div class="Modern-Slider">
-                                        <div class="item">
-                                            <div class="img-fill">
-                                                <img src="{{asset('assets/images/slide-01.jpg')}}" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="img-fill">
-                                                <img src="{{asset('assets/images/slide-02.jpg')}}" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="img-fill">
-                                                <img src="{{asset('assets/images/slide-03.jpg')}}" alt="">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    --***** Connexion Modal Ends ***** -->
-
 <!-- Sidescreen Starts Here-->
 @auth
 <div class="offcanvas offcanvas-end text-bg-dark" id="sideScreen">
@@ -181,8 +134,8 @@
                 <div class="left-content">
                     <div class="inner-content">
                         <h4>Restau-U</h4>
-                        <h6>Plus simple &amp; Plus efficace</h6>
-                        < livewire:button-reserver />
+                        <h6 class="pb-3">Plus simple &amp; Plus efficace</h6>
+                        <livewire:button-reserver />
                     </div>
                 </div>
             </div>
@@ -209,59 +162,8 @@
             </div>
         </div>
     </div>
-    <!-- ***** Reservation Us Area Starts ***** 
-    <section class="collapse" id="reservation">
-        <div class="section container-fluid has-bg-overlay text-center text-light has-height-md middle-items">
-            < livewire:ticket-form />
-        </div>
-    </section>
-    ***** Reservation Area Ends ***** -->
 </div>
-
-
 <!-- ***** Main Banner Area End ***** -->
-
-<!-- ***** About Area Starts ***** -->
-<section class="section" id="about">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-xs-12">
-                <div class="left-text-content">
-                    <div class="section-heading">
-                        <h6>About Us</h6>
-                        <h2>We Leave A Delicious Memory For You</h2>
-                    </div>
-                    <p>Klassy Cafe is one of the best restaurant HTML templates</a> with Bootstrap v4.5.2 CSS
-                        framework. You can download and feel free to use this website template layout for your
-                        restaurant business. You are allowed to use this template for commercial purposes.
-                        <br><br>You are NOT allowed to redistribute the template ZIP file on any template donwnload
-                        website. Please contact us for more information.
-                    </p>
-                    <div class="row">
-                        <div class="col-4">
-                            <img src="{{asset('assets/images/about-thumb-01.jpg')}}" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img src="{{asset('assets/images/about-thumb-02.jpg')}}" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img src="{{asset('assets/images/about-thumb-03.jpg')}}" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-xs-12">
-                <div class="right-content">
-                    <div class="thumb">
-                        <a rel="nofollow" href="http://youtube.com"><i class="fa fa-play"></i></a>
-                        <img src="{{asset('assets/images/about-video-bg.jpg')}}" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- ***** About Area Ends ***** -->
 
 <!-- ***** Menu Area Starts ***** -->
 <section class="section" id="chefs">
@@ -313,9 +215,49 @@
         </div>
     </div>
 </section>
-
 <!-- ***** Menu Area Ends ***** -->
 
+<!-- ***** About Area Starts ***** -->
+<section class="section" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <div class="left-text-content">
+                    <div class="section-heading">
+                        <h6>About Us</h6>
+                        <h2>We Leave A Delicious Memory For You</h2>
+                    </div>
+                    <p>Klassy Cafe is one of the best restaurant HTML templates</a> with Bootstrap v4.5.2 CSS
+                        framework. You can download and feel free to use this website template layout for your
+                        restaurant business. You are allowed to use this template for commercial purposes.
+                        <br><br>You are NOT allowed to redistribute the template ZIP file on any template donwnload
+                        website. Please contact us for more information.
+                    </p>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="{{asset('assets/images/about-thumb-01.jpg')}}" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="{{asset('assets/images/about-thumb-02.jpg')}}" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img src="{{asset('assets/images/about-thumb-03.jpg')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <div class="right-content">
+                    <div class="thumb">
+                        <a rel="nofollow" href="http://youtube.com"><i class="fa fa-play"></i></a>
+                        <img src="{{asset('assets/images/about-video-bg.jpg')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ***** About Area Ends *****   -->
 
 <!-- ***** Authentication Required Error Start ***** -->
 <div class="modal" tabindex="-1" id="authRequired" data-backdrop="static">
